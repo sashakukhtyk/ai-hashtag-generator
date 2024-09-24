f4e71751d286842af1d7ed41309d413af825c66f
sashakukhtyk <sashakukhtyk@users.noreply.github.com>
2024-09-24 12:03:23 +0300

    add the first function that encode image to base64 str

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
+"""
+This is the main logic of the programm
+
+"""
 import base64
 import requests
+
+
+def encode_image(image_path):
+    """
+    This function takes path to the image and return a base64 version
+    of it
+    
+    Args:
+        image_path (str): path to the image
+
+    Returns:
+        str: base64
+    """
+    with open(image_path, 'rb') as f:
+        return base64.b64encode(f.read()).decode('utf-8')
+    
+
