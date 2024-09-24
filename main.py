a887357ccef83c79fc45ba2728749acf3012bb39
sashakukhtyk <sashakukhtyk@users.noreply.github.com>
2024-09-24 12:35:05 +0300

    added the load env part and load openai key

--- main.py
+++ main.py
@@ -2,8 +2,14 @@
 This is the main logic of the programm
 
 """
+import os
 import base64
 import requests
+from dotenv import load_dotenv
+
+
+# Load .env file with OpenAI API key
+load_dotenv()
 
 
 def encode_image(image_path):
@@ -21,3 +27,9 @@ def encode_image(image_path):
         return base64.b64encode(f.read()).decode('utf-8')
     
 
+image_base64 = encode_image('images/test_image01.jpeg')
+
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {os.getenv("OPENAI_KEY")}'
+}
